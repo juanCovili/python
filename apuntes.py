294c86cccad847cb746dea2346642b7ae7b86540
Juan Covili <juancovrex@gmail.com>
2023-09-05 22:43:53 -0300

    Added operators and function type

---
+++ apuntes.py
@@ -0,0 +1,11 @@
+# tipos de datos: numericos: enteros int, coma flotante float, complejos ; textos ; booleanos: true, false
+# operadores: 
+# git vs code: https://www.youtube.com/watch?v=lYiE5lBS13E
+print(5+6)
+print(10%3)
+print(5**3)
+print(9//2)
+numero=5
+print(type(numero))
+nombre="juan"
+print(type(nombre))
